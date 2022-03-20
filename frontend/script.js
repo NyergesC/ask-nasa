e855df9be3fbd3bf0e0f079fc4fd49cd37901082
NyergesC <cintia.nyerges@gmail.com>
2022-03-20 19:21:22 +0100

    finished

--- frontend/script.js
+++ frontend/script.js
@@ -1,36 +1,61 @@
-const loadEvent = async _ => {
+const loadEvent = async _ => {    
     
-    const apod = await fetch("https://api.nasa.gov/planetary/apod?api_key=4WFi6RaKmXqC6eJlSWwFDzKAgdcn8TlLCxloTi14");
-
-    const apodJson = await apod.json()
-
-    console.log(apodJson)
-
     const rootElement = document.getElementById("root");
-
-    const title = apodJson.title
-    const image = apodJson.url
-    const explanation = apodJson.explanation
-
-    const todayPost = (title, image, explanation) => {
-        return `
-        <section>
-            <div>
-                <input type="date" id="date"></input>
-                <h1>${title}</h1>
-                <img src="${image}"></img>
-                <p>${explanation}</p>           
-            </div>
-        </section>
-        `
-    };     
     
-    rootElement.insertAdjacentHTML("beforeend", todayPost(title, image, explanation))
+    const currentD = new Date().toISOString().split("T")[0]
+
+    rootElement.insertAdjacentHTML("beforeend", `
+    <input class="test "type="date" id="date" value="${currentD}"> `)
     
     const date = document.getElementById("date");
-  
-    date.addEventListener('input', function(e) {
-      console.log(e.target.value)
-    });
+
+    createPost()
+
+    
+    date.addEventListener('input', async e => {
+        console.log(e.target.value)
+ 
+        createPost()
+        
+    })
+
+    async function createPost () {
+        const apiKey = "4WFi6RaKmXqC6eJlSWwFDzKAgdcn8TlLCxloTi14";
+        const reqDate = document.getElementById('date').value
+        
+        const apod = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${reqDate}`)
+        const apodJson = await apod.json()    
+        console.log(apodJson)
+
+        const title = apodJson.title
+        const image = apodJson.url
+        const explanation = apodJson.explanation
+
+        
+        console.log(title)        
+        console.log(explanation)        
+        
+        const todayPost = (title, image, explanation) => {
+            return `
+            <section id='macska'>
+            <div>
+            <h1>${title}</h1>
+            <img src="${image}"></img>
+            <p>${explanation}</p>           
+            </div>
+            </section>
+            `
+        }
+
+            const macskaElement = document.getElementById('macska')
+
+            if(macskaElement != null){
+                macskaElement.remove()
+            }            
+            rootElement.insertAdjacentHTML("beforeend", todayPost(title, image, explanation))               
+
+    }
+
+    
 }
 window.addEventListener("load", loadEvent);
